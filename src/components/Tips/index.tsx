87451305e5834f838237c03709ea06f090ec4541
Hugo Lima Romão <hugo8romao@gmail.com>
2024-02-22 16:35:45 -0400

    feat: Tips skeleton

--- src/components/Tips/index.tsx
+++ src/components/Tips/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { Card } from "@nextui-org/react";
+import { Card, Skeleton } from "@nextui-org/react";
 import { motion, AnimatePresence } from "framer-motion";
 
 const Tips = () => {
@@ -19,9 +19,11 @@ const Tips = () => {
 
   useEffect(() => {
     getNewTip();
-    setInterval(getNewTip, 10000);
+    setInterval(getNewTip, 15000);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  if (tipIndex === -1) return <Skeleton className="h-40 rounded-medium" />;
 
   return (
     <div className="flex flex-col gap-2">
