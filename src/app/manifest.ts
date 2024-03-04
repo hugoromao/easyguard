9c40340436cd0a424bc496546a45ba2006054e6b
Hugo Lima Romão <hugo8romao@gmail.com>
2024-03-04 13:20:45 -0400

    fix: adding types to manifest

---
+++ src/app/manifest.ts
@@ -0,0 +1,30 @@
+import { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Gerador de Senhas Gamificado",
+    icons: [
+      {
+        src: "/icons/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "any",
+      },
+      {
+        src: "/icons/android-chrome-384x384.png",
+        sizes: "384x384",
+        type: "image/png",
+      },
+      {
+        src: "/icons/icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+    theme_color: "#185449",
+    background_color: "#FAFAFA",
+    start_url: "/",
+    display: "standalone",
+    orientation: "portrait",
+  };
+}
