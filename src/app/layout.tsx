f4017d30c6edd88d74291e8dd1e99f0693be9be7
Hugo Lima Romão <hugo8romao@gmail.com>
2024-03-05 09:27:34 -0400

    feat: hotjar config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,23 +23,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR">
-      <Script
-        id="HotJarAnalytics"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `<!-- Hotjar Tracking Code for https://gamified-password-generator.vercel.app -->
-        <script>
-            (function(h,o,t,j,a,r){
-                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                h._hjSettings={hjid:${process.env.HJID},hjsv:6};
-                a=o.getElementsByTagName('head')[0];
-                r=o.createElement('script');r.async=1;
-                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                a.appendChild(r);
-            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-        </script>`,
-        }}
-      />
+      {process.env.NODE_ENV === "production" ? (
+        <Script
+          id="HotJarAnalytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `<script>(function(h,o,t,j,a,r){h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};h._hjSettings={hjid:${process.env.HJID},hjsv:6};a=o.getElementsByTagName('head')[0];r=o.createElement('script');r.async=1;r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;a.appendChild(r);})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');</script>`,
+          }}
+        />
+      ) : null}
       <body>
         <Providers>{children}</Providers>
       </body>
